e5aa48f319390da96ca0bc0dd71a2811e3cbc4e0
Mohamed <1726221@brunel.ac.uk>
2021-01-16 16:39:17 +0000

    new Commit

---
+++ BetterLifeFinal/Views/Food Intolerance screens /Vegeterian/VegeterianBasicView.swift
@@ -0,0 +1,188 @@
+//
+//  VegeterianBasicView.swift
+//  BetterLifeFinal
+//
+//  Created by Mohamed on 16/01/2021.
+//
+
+import UIKit
+
+class VegeterianBasicView: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    @IBOutlet weak var breakfastCollectionView: UICollectionView!
+    
+    @IBOutlet weak var lunchCollectionView: UICollectionView!
+    
+    @IBOutlet weak var dinnerCollectionView: UICollectionView!
+    
+    let breakfastLabels = ["Veggie", "2", "3", "4", "5"]
+    let lunchLabels = ["1- high calorie","2", "3", "4", "5"]
+    let dinnerLabels = ["vegan", "apple", "shake", "avacado", "rice", "last"]
+    
+    var breakfastItems = [[String:String]]()
+    var lunchItems = [[String:String]]()
+    var dinnerItems = [[String:String]]()
+    
+    func vegeterianBreakfastPlist () -> [[String:String]] {
+        
+        let path = Bundle.main.path(forResource: "VegeterianBreakfast", ofType: "plist")
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path!)) as! [[String:String]]
+        
+        
+    }
+    func vegeterianLunchPlist() -> [[String:String]] {
+        
+        let path1 = Bundle.main.path (forResource: "VegeterianLunch", ofType: "plist")
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path1!)) as! [[String:String]]
+        
+    }
+    func vegeterianDinnerPlist() -> [[String:String]] {
+        
+        let path2 = Bundle.main.path (forResource: "VegeterianDinner", ofType: "plist")
+        
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path2!)) as! [[String:String]]
+    }
+    
+    override func viewDidLoad() {
+        // load Breakfast images
+        self.breakfastItems = vegeterianBreakfastPlist()
+        // load Lunch Images
+        self.lunchItems = vegeterianLunchPlist()
+        // load dinner images
+        self.dinnerItems = vegeterianDinnerPlist()
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "vegBreakfastToRecipe" {
+           
+            let detail = segue.destination as! RecipeForNothing
+        
+            if let indexPath = self.breakfastCollectionView?.indexPath(for: sender as! UICollectionViewCell) {
+                
+                let breakfastItem = self.breakfastItems [indexPath.row]
+           
+                detail.sentData1 = breakfastItem ["Title"]
+                detail.sentData2 = breakfastItem ["Recipe"]
+                detail.sentData3 = breakfastItem ["Image"]
+                detail.sentData4 = breakfastItem ["Description"]
+            }
+        }
+        if segue.identifier == "vegLunchToRecipe" {
+               
+                let detail = segue.destination as! RecipeForNothing
+            
+                if let indexPath = self.lunchCollectionView?.indexPath(for: sender as! UICollectionViewCell) {
+                    
+                    let lunchItem = self.lunchItems[indexPath.row]
+               
+                    detail.sentData1 = lunchItem ["Title"]
+                    detail.sentData2 = lunchItem ["Recipe"]
+                    detail.sentData3 = lunchItem ["Image"]
+                    detail.sentData4 = lunchItem ["Description"]
+                }
+        }
+        if segue.identifier == "vegDinnerToRecipe" {
+           
+            let detail = segue.destination as! RecipeForNothing
+        
+            if let indexPath = self.dinnerCollectionView?.indexPath(for: sender as! UICollectionViewCell) {
+                
+                let dinnerItem = self.dinnerItems[indexPath.row]
+           
+                detail.sentData1 = dinnerItem ["Title"]
+                detail.sentData2 = dinnerItem ["Recipe"]
+                detail.sentData3 = dinnerItem ["Image"]
+                detail.sentData4 = dinnerItem ["Description"]
+            }
+        }
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if (collectionView == breakfastCollectionView) {
+            return 1
+        }
+        if (collectionView == lunchCollectionView) {
+            return 1
+        }
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (collectionView == breakfastCollectionView) {
+            return self.breakfastItems.count
+        }
+        if (collectionView == lunchCollectionView) {
+            return self.lunchItems.count
+        }
+        return self.dinnerItems.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if (collectionView == breakfastCollectionView) {
+            
+            let breakfastCell = breakfastCollectionView.dequeueReusableCell(withReuseIdentifier: "BreakfastCell", for: indexPath) as! VegeterianImageCollectionView
+            
+            let breakfastItem = self.breakfastItems[indexPath.row]
+            
+            breakfastCell.vegeterianBreakfastTextLabel.text = breakfastLabels[indexPath.row]
+            breakfastCell.vegeterianBreakfastImage.image = UIImage(named: breakfastItem ["Image"]!)
+            
+            breakfastCell.layer.borderColor = UIColor.lightGray.cgColor
+            breakfastCell.layer.borderWidth = 0.5
+            
+            return breakfastCell
+        }
+        if (collectionView == lunchCollectionView) {
+            let lunchCell = lunchCollectionView.dequeueReusableCell(withReuseIdentifier: "LunchCell", for: indexPath) as! VegeterianImageCollectionView
+            
+            
+            let lunchitem = self.lunchItems[indexPath.row]
+            
+            lunchCell.vegeterianLunchTextLabel.text = lunchLabels[indexPath.row]
+            lunchCell.vegeterianLunchImage.image = UIImage(named: lunchitem ["Image"]!)
+            
+            lunchCell.layer.borderColor = UIColor.lightGray.cgColor
+            lunchCell.layer.borderWidth = 0.5
+            
+            return lunchCell
+        }
+        let dinnerCell = dinnerCollectionView.dequeueReusableCell(withReuseIdentifier: "DinnerCell", for: indexPath) as! VegeterianImageCollectionView
+        
+        let dinnerItem = self.dinnerItems[indexPath.row]
+        
+        dinnerCell.vegeterianDinnerTextLabel.text = dinnerLabels[indexPath.row]
+        dinnerCell.vegeterianDinnerImage.image = UIImage (named: dinnerItem ["Image"]!)
+        
+        dinnerCell.layer.borderColor = UIColor.lightGray.cgColor
+        dinnerCell.layer.borderWidth = 0.5
+        
+        return dinnerCell
+    
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if (collectionView == breakfastCollectionView) {
+            
+            let breakfastCell = breakfastCollectionView.cellForItem(at: indexPath)
+            breakfastCell?.layer.borderColor = UIColor.lightGray.cgColor
+            breakfastCell?.layer.borderWidth = 0.5
+        }
+        if (collectionView == lunchCollectionView) {
+            
+            let lunchCell = lunchCollectionView.cellForItem(at: indexPath)
+            lunchCell?.layer.borderColor = UIColor.lightGray.cgColor
+            lunchCell?.layer.borderWidth = 0.5
+            
+        }
+        if (collectionView == dinnerCollectionView) {
+            
+            let dinnerCell = dinnerCollectionView.cellForItem(at: indexPath)
+            dinnerCell?.layer.borderColor = UIColor.lightGray.cgColor
+            dinnerCell?.layer.borderWidth = 0.5
+            
+        }
+    }
+    
+}
---
+++ BetterLifeFinal/Views/Food Intolerance screens /Vegeterian/VegeterianImageCollectionView.swift
@@ -0,0 +1,26 @@
+//
+//  VegeterianImageCollectionView.swift
+//  BetterLifeFinal
+//
+//  Created by Mohamed on 16/01/2021.
+//
+
+import UIKit
+
+class VegeterianImageCollectionView:UICollectionViewCell {
+    
+    
+    @IBOutlet weak var vegeterianBreakfastImage: UIImageView!
+    
+    @IBOutlet weak var vegeterianBreakfastTextLabel: UILabel!
+    
+    @IBOutlet weak var vegeterianLunchImage: UIImageView!
+    
+    @IBOutlet weak var vegeterianLunchTextLabel: UILabel!
+    
+    @IBOutlet weak var vegeterianDinnerImage: UIImageView!
+    
+    @IBOutlet weak var vegeterianDinnerTextLabel: UILabel!
+    
+    
+}
