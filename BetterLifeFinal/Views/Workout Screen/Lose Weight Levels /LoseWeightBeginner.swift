62886de4b5d4e1d6defb8c9b9282bd985a28dbfa
Mohamed <1726221@brunel.ac.uk>
2021-01-24 19:16:24 +0000

    added a new Workout section (Lose Weight)

---
+++ BetterLifeFinal/Views/Workout Screen/Lose Weight Levels /LoseWeightBeginner.swift
@@ -0,0 +1,148 @@
+//
+//  LoseWeightBeginner.swift
+//  BetterLifeFinal
+//
+//  Created by Mohamed on 23/01/2021.
+//
+
+import UIKit
+
+class LoseWeightBeginner:UIViewController,UICollectionViewDelegate,UICollectionViewDataSource  {
+    
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "beginnerBackToHome", sender: self)
+    }
+    
+    
+    @IBOutlet weak var firstWeekCollectionView: UICollectionView!
+    
+    @IBOutlet weak var secondWeekCollectionView: UICollectionView!
+    
+    @IBOutlet weak var thirdWeekCollectionView: UICollectionView!
+    
+    let firstWeekLabels = ["Day 1", "Day 2", "Day 3", "Day 4"]
+    let secondWeekLabels = ["Day 1", "Day 2", "Day 3", "Day 4"]
+    let thirdWeekLabels = ["Day 1", "Day 2", "Day 3", "Day 4"]
+    
+    var firstWeekItems = [[String:String]]()
+    var secondWeekItems = [[String:String]]()
+    var thirdWeekItems = [[String:String]]()
+    
+    func BeginnerLoseWeightPlist () -> [[String:String]] {
+        
+        let path = Bundle.main.path(forResource: "BeginnerLoseWeight", ofType: "plist")
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path!)) as! [[String:String]]
+        
+    }
+    func LoseWeightSecondWeekPlist () -> [[String:String]] {
+        
+        let path1 = Bundle.main.path(forResource: "LoseWeightSecondWeek", ofType: "plist")
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path1!)) as! [[String:String]]
+        
+    }
+    func LoseWeightThirdWeekPlist () -> [[String:String]] {
+        
+        let path2 = Bundle.main.path(forResource: "LoseWeightThirdWeek", ofType: "plist")
+        
+        return NSArray.init(contentsOf: URL.init (fileURLWithPath: path2!)) as! [[String:String]]
+        
+    }
+
+    
+    override func viewDidLoad() {
+        // load  images
+        self.firstWeekItems = BeginnerLoseWeightPlist()
+        
+        self.secondWeekItems = LoseWeightSecondWeekPlist()
+        
+        self.thirdWeekItems = LoseWeightThirdWeekPlist()
+     
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        if (collectionView == firstWeekCollectionView) {
+            return 1
+        }
+        if (collectionView == secondWeekCollectionView) {
+            return 1
+        }
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if (collectionView == firstWeekCollectionView) {
+            return self.firstWeekItems.count
+        }
+        if (collectionView == secondWeekCollectionView) {
+            return self.secondWeekItems.count
+        }
+        return self.thirdWeekItems.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if (collectionView == firstWeekCollectionView) {
+            
+            let WeekOneCell = firstWeekCollectionView.dequeueReusableCell(withReuseIdentifier: "WeekOneCell", for: indexPath) as! LoseWeightImageCollectionView
+            
+            let firstWeekItem = self.firstWeekItems[indexPath.row]
+            
+            WeekOneCell.firstWeekTextLabel.text = firstWeekLabels[indexPath.row]
+            WeekOneCell.firstWeekImage.image = UIImage(named:firstWeekItem ["Image"]!)
+            
+            WeekOneCell.layer.borderColor = UIColor.lightGray.cgColor
+            WeekOneCell.layer.borderWidth = 0.5
+            
+            return WeekOneCell
+        }
+        if (collectionView == secondWeekCollectionView) {
+            
+            let WeekTwoCell = secondWeekCollectionView.dequeueReusableCell(withReuseIdentifier: "WeekTwoCell", for: indexPath) as! LoseWeightImageCollectionView
+            
+            let secondWeekItem = self.secondWeekItems[indexPath.row]
+            
+            WeekTwoCell.secondWeekTextLabel.text = secondWeekLabels[indexPath.row]
+            WeekTwoCell.secondWeekImage.image = UIImage(named: secondWeekItem ["Image"]!)
+            
+            WeekTwoCell.layer.borderColor = UIColor.lightGray.cgColor
+            WeekTwoCell.layer.borderWidth = 0.5
+            
+            return WeekTwoCell
+        }
+        let WeekThreeCell = thirdWeekCollectionView.dequeueReusableCell(withReuseIdentifier: "WeekThreeCell", for: indexPath) as! LoseWeightImageCollectionView
+        
+        let thirdWeekItem = self.thirdWeekItems[indexPath.row]
+        
+        WeekThreeCell.thirdWeekTextLabel.text = secondWeekLabels[indexPath.row]
+        WeekThreeCell.thirdWeekImage.image = UIImage(named: thirdWeekItem ["Image"]!)
+        
+        WeekThreeCell.layer.borderColor = UIColor.lightGray.cgColor
+        WeekThreeCell.layer.borderWidth = 0.5
+        
+        return WeekThreeCell
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if (collectionView == firstWeekCollectionView) {
+            
+            let WeekOneCell = firstWeekCollectionView.cellForItem(at: indexPath)
+            WeekOneCell?.layer.borderColor = UIColor.lightGray.cgColor
+            WeekOneCell?.layer.borderWidth = 0.5
+        }
+        if (collectionView == secondWeekCollectionView) {
+            
+            let WeekTwoCell = secondWeekCollectionView.cellForItem(at: indexPath)
+            WeekTwoCell?.layer.borderColor = UIColor.lightGray.cgColor
+            WeekTwoCell?.layer.borderWidth = 0.5
+            
+        }
+        if (collectionView == thirdWeekCollectionView) {
+            
+            let WeekThreeCell = thirdWeekCollectionView.cellForItem(at: indexPath)
+            WeekThreeCell?.layer.borderColor = UIColor.lightGray.cgColor
+            WeekThreeCell?.layer.borderWidth = 0.5
+            
+        }
+    }
+}
---
+++ BetterLifeFinal/Views/Workout Screen/Lose Weight Levels /LoseWeightImageCollectionView.swift
@@ -0,0 +1,25 @@
+//
+//  LoseWeightImageCollectionView.swift
+//  BetterLifeFinal
+//
+//  Created by Mohamed on 24/01/2021.
+//
+
+import UIKit
+
+class LoseWeightImageCollectionView:UICollectionViewCell {
+    
+    
+    @IBOutlet weak var firstWeekImage: UIImageView!
+    
+    @IBOutlet weak var firstWeekTextLabel: UILabel!
+    
+    @IBOutlet weak var secondWeekImage: UIImageView!
+    
+    @IBOutlet weak var secondWeekTextLabel: UILabel!
+    
+    @IBOutlet weak var thirdWeekImage: UIImageView!
+    
+    @IBOutlet weak var thirdWeekTextLabel: UILabel!
+    
+}
